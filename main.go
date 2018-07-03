dfa90deff022ccdc4dc6c0a38d0e294d74ca29b4
skylerlee <skyler.ac.lee@gmail.com>
2018-07-03 23:22:47 +0800

    feat: make backend to read from fifo pipe

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 )
 
 func start() {
-	scanner := bufio.NewScanner(os.Stdin)
+	fp, err := os.Open("histlite.pipe")
+	if err != nil {
+		fmt.Println("fifo pipe not exists")
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println(line)
