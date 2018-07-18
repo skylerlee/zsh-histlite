9ee3735f84f718f607cd8e318fcd2910f5cd4c44
skylerlee <skyler.ac.lee@gmail.com>
2018-07-18 22:55:43 +0800

    feat: add db creation and insertion functions

--- histlite.go
+++ histlite.go
@@ -9,5 +9,22 @@ const (
 	HistoryFile = ".zsh_history.db"
 )
 
+func CreateTable(db *sql.DB) {
+	db.Exec(`CREATE TABLE hl_history (
+		id INTEGER,
+		command TEXT,
+		retcode INTEGER,
+		timestamp TEXT
+	)`);
+}
+
+func InsertHistory(db *sql.DB) {
+	db.Exec(`INSERT INTO hl_history (
+		id, command, retcode, timestamp
+	) VALUES (
+		0, "test", 0, 0
+	)`);
+}
+
 func Test(line string) {
 }
