9dbaf84fd9e00dd7b2a4a529c138c013bc0889e7
skylerlee <skyler.ac.lee@gmail.com>
2018-07-19 23:40:08 +0800

    feat: add db initializer function

--- histlite.go
+++ histlite.go
@@ -1,6 +1,8 @@
 package histlite
 
 import (
+	"os"
+	"path/filepath"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -8,6 +10,12 @@ import (
 const (
 	HistoryFile = ".zsh_history.db"
 )
+
+func InitStorage() (*sql.DB, error) {
+	basePath := os.Getenv("HOME");
+	dbFile := filepath.Join(basePath, HistoryFile);
+	return sql.Open("sqlite3", dbFile);
+}
 
 func CreateTable(db *sql.DB) {
 	db.Exec(`CREATE TABLE hl_history (
