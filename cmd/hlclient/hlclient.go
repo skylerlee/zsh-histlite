b05cc63a182829b206be02f11de895838cb63a11
skylerlee <skyler.ac.lee@gmail.com>
2018-07-28 22:08:21 +0800

    feat: update command query flag

--- cmd/hlclient/hlclient.go
+++ cmd/hlclient/hlclient.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/skylerlee/zsh-histlite"
 )
 
+var command string
+
 var mainCmd = &cobra.Command{
 	Use: "hlclient",
 	Run: func(cmd *cobra.Command, args []string) {
+		if cmd.Flag("query").Changed {
+			fmt.Println(command)
+		}
 	},
+}
+
+func init() {
+	mainCmd.Flags().StringVarP(&command, "query", "q", "", "command prefix for query")
 }
 
 func main() {
