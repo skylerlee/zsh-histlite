88c094474809b19970b131cafbd677ac60d494ce
skylerlee <skyler.ac.lee@gmail.com>
2018-07-28 23:01:21 +0800

    feat: add more command flags

--- cmd/hlclient/hlclient.go
+++ cmd/hlclient/hlclient.go
@@ -12,13 +12,20 @@ var command string
 var mainCmd = &cobra.Command{
 	Use: "hlclient",
 	Run: func(cmd *cobra.Command, args []string) {
-		if cmd.Flag("query").Changed {
-			fmt.Println(command)
+		switch {
+		case cmd.Flag("add").Changed:
+			fmt.Println("add", command)
+		case cmd.Flag("drop").Changed:
+			fmt.Println("drop", command)
+		case cmd.Flag("query").Changed:
+			fmt.Println("query", command)
 		}
 	},
 }
 
 func init() {
+	mainCmd.Flags().StringVarP(&command, "add", "a", "", "command to add to history")
+	mainCmd.Flags().StringVarP(&command, "drop", "d", "", "command to drop from history")
 	mainCmd.Flags().StringVarP(&command, "query", "q", "", "command prefix for query")
 }
 
