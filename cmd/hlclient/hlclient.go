d059b5ce0fc96f3addf815c533fb5eacb4d050b3
skylerlee <skyler.ac.lee@gmail.com>
2018-08-01 00:02:41 +0800

    feat: update main command function

--- cmd/hlclient/hlclient.go
+++ cmd/hlclient/hlclient.go
@@ -14,11 +14,11 @@ var mainCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		switch {
 		case cmd.Flag("add").Changed:
-			fmt.Println("add", command)
+			addCommand(command)
 		case cmd.Flag("drop").Changed:
-			fmt.Println("drop", command)
+			dropCommand(command)
 		case cmd.Flag("query").Changed:
-			fmt.Println("query", command)
+			queryCommand(command)
 		default:
 			cmd.Help()
 		}
@@ -29,6 +29,15 @@ func init() {
 	mainCmd.Flags().StringVarP(&command, "add", "a", "", "add command to history")
 	mainCmd.Flags().StringVarP(&command, "drop", "d", "", "drop command from history")
 	mainCmd.Flags().StringVarP(&command, "query", "q", "", "query command by prefix")
+}
+
+func addCommand(line string) {
+}
+
+func dropCommand(line string) {
+}
+
+func queryCommand(line string) {
 }
 
 func main() {
