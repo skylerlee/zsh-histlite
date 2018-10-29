3c17dea470a6eac46a9f455950e5da46e46fe379
skylerlee <skyler.ac.lee@gmail.com>
2018-10-30 00:20:01 +0800

    feat: use builtin flag package for argument parsing

--- cmd/hlclient/hlclient.go
+++ cmd/hlclient/hlclient.go
@@ -1,40 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/skylerlee/zsh-histlite"
 )
 
 var (
-	command string
-	offset  int
+	addString   string
+	dropString  string
+	queryString string
+	offset      int
 )
 
-var mainCmd = &cobra.Command{
-	Use: "hlclient",
-	Run: func(cmd *cobra.Command, args []string) {
-		switch {
-		case cmd.Flag("add").Changed:
-			addCommand(command)
-		case cmd.Flag("drop").Changed:
-			dropCommand(command)
-		case cmd.Flag("query").Changed:
-			queryCommand(command)
-		default:
-			cmd.Help()
-		}
-	},
-}
-
 func init() {
-	flags := mainCmd.Flags()
-	flags.StringVarP(&command, "add", "a", "", "add command to history")
-	flags.StringVarP(&command, "drop", "d", "", "drop command from history")
-	flags.StringVarP(&command, "query", "q", "", "query command by prefix")
-	flags.IntVarP(&offset, "number", "n", 0, "command offset")
+	flag.StringVar(&addString, "a", "", "add command to history")
+	flag.StringVar(&dropString, "d", "", "drop command from history")
+	flag.StringVar(&queryString, "q", "", "query command by prefix")
+	flag.IntVar(&offset, "n", 0, "command offset")
 }
 
 func preflight(ctx *histlite.Context) {
@@ -70,7 +55,13 @@ func queryCommand(line string) {
 }
 
 func main() {
-	if err := mainCmd.Execute(); err != nil {
-		os.Exit(histlite.ERR_EXC)
+	flag.Parse()
+	switch {
+	case addString != "":
+		addCommand(addString)
+	case dropString != "":
+		dropCommand(dropString)
+	case queryString != "":
+		queryCommand(queryString)
 	}
 }
