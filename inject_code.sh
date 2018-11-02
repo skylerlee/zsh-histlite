b6eb94834024961d3cb86bc9c935157e6378b761
skylerlee <skyler.ac.lee@gmail.com>
2018-11-03 02:40:57 +0800

    feat: update inject script

--- inject_code.sh
+++ inject_code.sh
@@ -9,11 +9,13 @@ function scan_keys {
   grep -Po '(?<={{)\w+(?=}})' $src
 }
 
+# find line number by pattern
 function find_line {
   local out=$(grep -Pon "\-\-\s*$1:\s*$2\s*\-\-" $dat)
   echo ${out%%:*}
 }
 
+# clip code block by key
 function clip_code {
   local key=$1
   local begin=$(find_line BEGIN $key)
@@ -21,15 +23,19 @@ function clip_code {
   sed -n "$((begin + 1)),$((end - 1))p" $dat
 }
 
-function inject_code {
-  echo $2
+function inject_code_blocks {
+  for key in ${!code_blocks[@]}; do
+    local code=${code_blocks[$key]}
+  done
 }
 
 function main {
+  declare -gA code_blocks=()
   for key in $(scan_keys); do
     local code=$(clip_code $key)
-    inject_code $key "$code"
+    code_blocks[$key]="$code"
   done
+  inject_code_blocks
 }
 
 main
